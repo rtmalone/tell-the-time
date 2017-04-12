a7ec89a32a1812fb68aa2bf16d66e5b3f2764b0b
Tyler Malone <rtylermalone@gmail.com>
2017-04-11 22:16:32 -0400

    turned LevelBtns into Class

--- src/components/ClockHand/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ClockHand = () => {
-  return <div>clock hand</div>
-}
-
-export default ClockHand;
--- src/components/LevelBtns/index.js
+++ src/components/LevelBtns/index.js
@@ -4,17 +4,22 @@ import {Link} from 'react-router-dom';
 import {Button, H1} from '../StyledComponents';
 import './style.css';
 
-const LevelBtns = () => {
-  return (
-    <div className='levelBtns'>
-      <div>
-        <H1>Choose a level:</H1>
+class LevelBtns extends React.Component {
+  render() {
+    return (
+      <div className='levelBtns'>
+        <div>
+          <H1>Choose a level:</H1>
+        </div>
+        <Link to={{
+          pathname: '/level1',
+          player: this.props.player,
+        }}><Button>1</Button></Link>
+        <Button>2</Button>
+        <Button>3</Button>
       </div>
-      <Link to='/level1'><Button>1</Button></Link>
-      <Button>2</Button>
-      <Button>3</Button>
-    </div>
-  )
+    )
+  }
 }
 
 export default LevelBtns;
