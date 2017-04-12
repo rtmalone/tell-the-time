a1d10b09855c7fa2b2b3e532d7860c72c6e3fa9c
Tyler Malone <rtylermalone@gmail.com>
2017-04-11 22:17:05 -0400

    turned NameInput into Class

--- src/components/NameInput/index.js
+++ src/components/NameInput/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import {Input, H1} from '../StyledComponents';
 
-const NameInput = () => {
-  return (
-    <div>
-      <H1>What's your name?</H1>
-      <Input type='text' height='50px' width='570px' />
-    </div>
-  )
+class NameInput extends React.Component {
+  handleChange = (event) => {
+    this.props.setPlayer(event.target.value);
+  }
+  
+  render() {
+    return (
+      <div>
+        <H1>What's your name?</H1>
+        <Input type='text' height='50px' width='570px' onChange={this.handleChange}/>
+      </div>
+    )
+  }
 }
 
 export default NameInput;
