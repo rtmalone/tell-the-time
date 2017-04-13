a1a3069e2ac83051c3ea21074dd65e424566fe70
Tyler Malone <rtylermalone@gmail.com>
2017-04-12 22:41:59 -0400

    refactor AnswerBtns

--- src/components/AnswerBtns/index.js
+++ src/components/AnswerBtns/index.js
@@ -4,13 +4,13 @@ import {Button, H1} from '../StyledComponents';
 import './style.css';
 
 const AnswerBtns = (props) => {
-  const {choices} = props
+  const { choices } = props;
   return (
     <div className='answers'>
       <H1>What time is it?</H1>
-      <Button inverse>{choices[0].time}</Button>
-      <Button inverse>{choices[1].time}</Button>
-      <Button inverse>{choices[2].time}</Button>
+      <Button onClick={props.checkAnswer} value={choices[0].time} inverse>{choices[0].time}</Button>
+      <Button onClick={props.checkAnswer} value={choices[1].time} inverse>{choices[1].time}</Button>
+      <Button onClick={props.checkAnswer} value={choices[2].time} inverse>{choices[2].time}</Button>
     </div>
   )
 }
