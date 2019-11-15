1181ce32dcd1c58c4bfde2f833f14c13e6802812
Tyler Malone <tyler@pylon.com>
2019-11-15 12:03:29 -0500

    Adds prettier options; update deps

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import  {BrowserRouter, Switch, Route} from 'react-router-dom';
-// import logo from './logo.svg';
-import './App.css';
-import '../static/simple-grid.min.css';
-// import Input from './components/NameInput';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+import './static/simple-grid.min.css'
 
-import Home from './containers/Home';
-import Level1 from './containers/Level1';
-import Level2 from './containers/Level2';
-import Level3 from './containers/Level3';
+import Home from './containers/Home'
+import Level1 from './containers/Level1'
+import Level2 from './containers/Level2'
+import Level3 from './containers/Level3'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className='App'>
+        <div className="App">
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/level1' component={Level1} />
-            <Route path='/level2' component={Level2} />
-            <Route path='/level3' component={Level3} />
+            <Route exact path="/" component={Home} />
+            <Route path="/level1" component={Level1} />
+            <Route path="/level2" component={Level2} />
+            <Route path="/level3" component={Level3} />
           </Switch>
         </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/containers/Level1/index.js
+++ src/containers/Level1/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-import Clock from '../../components/Clock';
-import AnswerBtns from '../../components/AnswerBtns';
-import './style.css';
+import Clock from '../../components/Clock'
+import AnswerBtns from '../../components/AnswerBtns'
+import './style.css'
 
 const hours = [
-  {time: '1:00', deg: 30},
-  {time: '2:00', deg: 60},
-  {time: '3:00', deg: 90},
-  {time: '4:00', deg: 120},
-  {time: '5:00', deg: 150},
-  {time: '6:00', deg: 180},
-  {time: '7:00', deg: 210},
-  {time: '8:00', deg: 240},
-  {time: '9:00', deg: 270},
-  {time: '10:00', deg: 300},
-  {time: '11:00', deg: 330},
-  {time: '12:00', deg: 0}
-];
+  { time: '1:00', deg: 30 },
+  { time: '2:00', deg: 60 },
+  { time: '3:00', deg: 90 },
+  { time: '4:00', deg: 120 },
+  { time: '5:00', deg: 150 },
+  { time: '6:00', deg: 180 },
+  { time: '7:00', deg: 210 },
+  { time: '8:00', deg: 240 },
+  { time: '9:00', deg: 270 },
+  { time: '10:00', deg: 300 },
+  { time: '11:00', deg: 330 },
+  { time: '12:00', deg: 0 }
+]
 
 class Level1 extends React.Component {
   constructor(props) {
@@ -26,18 +26,18 @@ class Level1 extends React.Component {
     this.state = {
       query: null,
       digits: [
-        {num: '1', translate:{ x:'4em' , y:'-7em'}},
-        {num: '2', translate:{ x:'7em' , y:'-4em'}},
-        {num: '3', translate:{ x:'8em' , y:'0em'}},
-        {num: '4', translate:{ x:'7em' , y:'4em'}},
-        {num: '5', translate:{ x:'4em' , y:'7em'}},
-        {num: '6', translate:{ x:'0em' , y:'8em'}},
-        {num: '7', translate:{ x:'-4em' , y:'7em'}},
-        {num: '8', translate:{ x:'-7em' , y:'4em'}},
-        {num: '9', translate:{ x:'-8em' , y:'0em'}},
-        {num: '10', translate:{ x:'-7em' , y:'-4em'}},
-        {num: '11', translate:{ x:'-4em' , y:'-7em'}},
-        {num: '12', translate:{ x:'0em' , y:'-8em'}},
+        { num: '1', translate: { x: '4em', y: '-7em' } },
+        { num: '2', translate: { x: '7em', y: '-4em' } },
+        { num: '3', translate: { x: '8em', y: '0em' } },
+        { num: '4', translate: { x: '7em', y: '4em' } },
+        { num: '5', translate: { x: '4em', y: '7em' } },
+        { num: '6', translate: { x: '0em', y: '8em' } },
+        { num: '7', translate: { x: '-4em', y: '7em' } },
+        { num: '8', translate: { x: '-7em', y: '4em' } },
+        { num: '9', translate: { x: '-8em', y: '0em' } },
+        { num: '10', translate: { x: '-7em', y: '-4em' } },
+        { num: '11', translate: { x: '-4em', y: '-7em' } },
+        { num: '12', translate: { x: '0em', y: '-8em' } }
       ],
       hours: null,
       choices: null,
@@ -46,65 +46,65 @@ class Level1 extends React.Component {
   }
 
   componentWillMount() {
-    const _array = this.shuffle(hours);
-    const _choices = _array.slice(0,3)
-    const index = Math.round(Math.random() * 2);
-    const _query = _choices[index];
+    const array = this.shuffle(hours)
+    const choices = array.slice(0, 3)
+    const index = Math.round(Math.random() * 2)
+    const query = choices[index]
     this.setState({
-      hours: _array,
-      query: _query,
-      choices: _choices
+      hours: array,
+      query,
+      choices
     })
   }
 
   chooseTime = (array) => {
-    const num = Math.round(Math.random() * (array.length - 1));
-    this.setState({query: array[num]});
+    const num = Math.round(Math.random() * (array.length - 1))
+    this.setState({ query: array[num] })
   }
 
   shuffle = (array) => {
-    let _length = array.length, temp, i;
+    let _length = array.length,
+      temp,
+      i
 
     while (_length) {
-      i = Math.floor(Math.random() * _length--);
+      i = Math.floor(Math.random() * _length--)
 
-      temp = array[_length];
-      array[_length] = array[i];
-      array[i] = temp;
+      temp = array[_length]
+      array[_length] = array[i]
+      array[i] = temp
     }
 
-    return array;
+    return array
   }
 
   checkAnswer = (event) => {
-    console.log(event.target.value);
     if (event.target.value === this.state.query.time) {
-      console.log('hooray');
       this.setState({
         isCorrect: true
-      });
-      setTimeout(function(){window.location.reload();}, 2000);
+      })
+      setTimeout(function() {
+        window.location.reload()
+      }, 2000)
     } else {
-      console.log('boo');
+      console.log('boo')
     }
   }
 
   render() {
-    const {choices, query, isCorrect} = this.state
+    const { choices, query, isCorrect } = this.state
     return (
-      <div className='row'>
-        <div className='col-7'>
+      <div className="row">
+        <div className="col-7">
           <Clock {...this.state} />
         </div>
-        <div className='col-5'>
+        <div className="col-5">
           <AnswerBtns checkAnswer={this.checkAnswer} choices={choices} query={query} />
-          { isCorrect &&
-            <p>Yay, {this.props.location.state.player}!</p>
-          }
+          {isCorrect && <p>Yay, {this.props.location.state.player}!</p>}
         </div>
       </div>
     )
   }
 }
 
-export default Level1;
+export default Level1
